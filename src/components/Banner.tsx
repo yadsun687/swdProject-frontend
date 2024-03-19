82eb73c7c6073bca8f9708508e187ea3c52a289b
Star2578 <nuttykingger@gmail.com>
2024-03-19 21:45:10 +0700

    adjust homepage to be more hotel

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,7 +12,7 @@ export default function Banner(){
     const router = useRouter()
     const {data:session} = useSession();
     const imgSrcs = [
-                    "/img/cover.jpg" , 
+                    "/img/cover1.jpg" , 
                     "/img/cover2.jpg", 
                     "/img/cover3.jpg", 
                     "/img/cover4.jpg"
@@ -35,12 +35,12 @@ export default function Banner(){
             }
 
             <div className="absolute font-kanit top-20 left-1/2 -translate-x-1/2 pt-7 pb-4 pl-2 pr-2 w-[90%] text-center rounded-sm bg-white/40 backdrop-blur-md shadow-xl z-50">
-                <h1 className="text-5xl mb-5">Vaccine Service Center</h1>
-                <p className="text-2xl font-[300]">Protect yourself and your loved ones - take vaccine today for safe, reliable immunization against infectious diseases</p>
+                <h1 className="text-5xl mb-5">Hotel Booking</h1>
+                <p className="text-2xl font-[300]">Fast and Easy - For you to spend your time enjoying your vacation</p>
             </div>
 
             <button onClick={(e)=>{e.stopPropagation(); router.push("/hospital")}} className="absolute font-kanit font-[400] text-2xl right-10 bottom-10 w-fit h-fit p-3 rounded-lg bg-white/30 backdrop-blur-md shadow-xl z-40 hover:ring-2 hover:ring-slate-100">
-                Select Hospital
+                Select Hotel
             </button>
         </div>
     )
--- src/components/PromoteCard.tsx
+++ src/components/PromoteCard.tsx
@@ -13,9 +13,13 @@ export default function PromoteCard(){
 
     return(
         <div className="flex flex-row w-fit h-fit p-5 pr-32 gap-x-5 mx-auto mt-10 bg-slate-100 rounded-sm">
-            <VideoPlayer vdoSrc={"/video/getvaccine.mp4"} isPlaying={vdoPlay} />
-            <div className="flex flex-col flex-nowrap text-2xl font-semibold">
-                <div>Get Your vaccine today</div>
+            <VideoPlayer vdoSrc={"/video/enjoyYourTime.mp4"} isPlaying={vdoPlay} />
+            <div className="flex flex-col flex-nowrap">
+                <div className="text-2xl font-semibold">Book your room today</div>
+                <div className="text-md font-light mt-2">
+                    We know time is precious so our goal is to help you <br />
+                    create wonderful moments with your partner.
+                </div>
                 <button onClick={()=>setVdoPlay(prev=>!prev)}
                     className="rounded-2xl w-32 h-fit text-xl bg-blue-500 text-slate-100 mt-auto p-1 pl-6 pr-6 hover:scale-105 hover:bg-blue-600 transition-all">
                     {vdoPlay? "Pause":"Play"}
